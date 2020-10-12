918216250900a183a0ce54a71c1afb6e6372f990
Seth Glenn <Sbgees@yahoo.com>
2020-10-11 23:20:58 -0500

    Implemented day tracking timer, and rows for schedules.

---
+++ 05_third_party_apis/script.js
@@ -0,0 +1,12 @@
+
+// Variables
+var events = document.querySelectorAll(".event");
+var current = document.querySelector("#currentDay");
+// Day.js for time keeping
+var current = dayjs().hour(18);
+
+$("#currentDay").text(current);
+
+
+
+
