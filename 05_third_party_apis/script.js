065d1476967440f72992d50a4b136422788e1b29
Seth Glenn <Sbgees@yahoo.com>
2020-10-13 22:55:02 -0500

    Implementing local storage save on button click for scheduler

--- 05_third_party_apis/script.js
+++ 05_third_party_apis/script.js
@@ -2,11 +2,26 @@
 // Variables
 var events = document.querySelectorAll(".event");
 var current = document.querySelector("#currentDay");
+var input = document.querySelector("#formGroupExampleInput");
+var saveButton = document.querySelector(".btn-dark");
+
 // Day.js for time keeping
-var current = dayjs().hour(18);
-
-$("#currentDay").text(current);
+var current = dayjs().hour(12);
 
 
+$("#currentDay",).text(current);
 
+// Function to save data
 
+function saveDataRender (){
+    var schedule = localStorage.getItem("schedule");
+
+    if (!schedule){
+        return;
+    }  
+}
+saveButton.addEventListener("click", function(event) {
+    event.preventDefault();
+  
+    
+  });
