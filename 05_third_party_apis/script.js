4a6e0fdd0de3641d8a00a1a82cdec00cabbd0fac
Seth Glenn <Sbgees@yahoo.com>
2020-10-14 23:10:29 -0500

    Having trouble with local storage saves. Implemented syntax to change background colors based on time, fixed time formatting.

--- 05_third_party_apis/script.js
+++ 05_third_party_apis/script.js
@@ -2,26 +2,61 @@
 // Variables
 var events = document.querySelectorAll(".event");
 var current = document.querySelector("#currentDay");
-var input = document.querySelector("#formGroupExampleInput");
+var input = document.querySelector("#data");
 var saveButton = document.querySelector(".btn-dark");
 
+var tenEl = document.querySelector("#form1");
+var elevenEl = document.querySelector("#form2");
+var twelveEl = document.querySelector("#form3");
+var oneEl = document.querySelector("#form4");
+var twoEl = document.querySelector("#form5");
+var threeEl = document.querySelector("#form6");
+var fourEl = document.querySelector("#form7");
+var fiveEl = document.querySelector("#form8");
+
 // Day.js for time keeping
-var current = dayjs().hour(12);
+var current = dayjs().format("MM/DD/YYYY H:mm");
 
 
 $("#currentDay",).text(current);
 
 // Function to save data
 
-function saveDataRender (){
-    var schedule = localStorage.getItem("schedule");
+renderData();
 
-    if (!schedule){
-        return;
-    }  
-}
-saveButton.addEventListener("click", function(event) {
-    event.preventDefault();
-  
+function renderData(){
     
-  });
+    // var stor = localStorage.getItem("data");
+
+     var stor = JSON.parse(localStorage.getItem("data"));
+
+    document.getElementById("data").innerHTML = stor;
+}
+
+// on click function
+
+$(".btn-dark").on("click", function(event) {
+    event.preventDefault();
+
+    // input = document.getElementById("data");
+
+    var input = document.querySelector("#data").value;
+
+    localStorage.setItem("data", JSON.stringify(input));
+  
+    // localStorage.getItem(input);
+
+    console.log(input);
+    
+     renderData();
+    
+});
+
+//Change Colors of Blocks based on time
+
+if (dayjs().format("H") == $(input).attr("current-day"))
+    $(input).addClass("bg-secondary");
+else if (dayjs().format("H") > $(input).attr("current-day"))
+    $(input).addClass("bg-danger");
+else (dayjs().format("H") < $(input).attr("current-day"))
+    $(input).addClass("bg-success");
